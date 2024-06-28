64245f93b06e1b829d60adc2fea9c1ce8ff87101
Radmir.Baembitov <radmir.baembitov@jetbrains.com>
2024-06-28 15:17:04 +0200

    Add some errors

--- BadRules/Class1.cs
+++ BadRules/Class1.cs
@@ -4,11 +4,16 @@ public class Class1 {
     public void Method() {
         var myActions = new List<Action>();
         var myStrings = new List<string>() { "one", "two", "three" };
-
+        var x = new int();
+        if (1 + 3 == 4) x = 5;
+        else x = 4;
+        if (1 + 3 == 4) x = 6;
+                else x = 2;
         for (var i = 0; i < myStrings.Count; i++)
         {
             Action print = () => { Console.WriteLine(myStrings[i]); };
             myActions.Add(print);
+
         }
 
         myActions[0]();
