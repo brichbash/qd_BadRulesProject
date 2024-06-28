92d4c6d9948066603c39bd9d779ea415038583f9
Radmir.Baembitov <radmir.baembitov@jetbrains.com>
2024-06-28 15:32:23 +0200

    Return the problem

--- BadRules/Class1.cs
+++ BadRules/Class1.cs
@@ -7,7 +7,8 @@ public class Class1 {
         var x = new int();
         if (1 + 3 == 4) x = 5;
         else x = 4;
-
+        if (1 + 3 == 4) x = 6;
+                else x = 2;
         for (var i = 0; i < myStrings.Count; i++)
         {
             Action print = () => { Console.WriteLine(myStrings[i]); };
